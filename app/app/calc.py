10116fb2677654b4334630eae3f08916ea87072c
Sophie Sherman <sophie-sherman@hotmail.com>
2022-10-16 13:22:46 +1100

    Configured db

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """Add x and y and result the result"""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract x from y and result the result"""
+    return y - x
